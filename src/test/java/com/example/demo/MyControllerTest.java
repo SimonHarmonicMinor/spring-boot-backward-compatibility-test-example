decb1a25188400728bc15ff816169bba8eca7a96
Semyon Kirekov <semyon@kirekov.com>
2023-04-01 18:49:40 +0300

    MyController sends and receives java.util.Map

--- src/test/java/com/example/demo/MyControllerTest.java
+++ src/test/java/com/example/demo/MyControllerTest.java
@@ -4,13 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.example.demo.request.OrderRequestV1;
-import com.example.demo.request.OrderRequestV2;
-import com.example.demo.request.OrderRequestV3;
-import com.example.demo.response.OrderResponseV1;
-import com.example.demo.response.OrderResponseV2;
-import com.example.demo.response.OrderResponseV3;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,42 +27,54 @@ class MyControllerTest {
     // Вызываем v3 так, как будто это v3
     final var responseV3 = rest.postForEntity(
         "/api/order/v3",
-        new OrderRequestV3("myOrder", List.of("item1", "item2")),
-        OrderResponseV3.class
+        Map.of(
+            "name", "myOrder",
+            "items", List.of("item1", "item2")
+        ),
+        Map.class
     );
-    assertTrue(responseV3.getStatusCode().is2xxSuccessful(), "Unexpected status code: " + responseV3.getStatusCode());
+    assertTrue(responseV3.getStatusCode().is2xxSuccessful(),
+        "Unexpected status code: " + responseV3.getStatusCode());
     final var orderResponseV3 = responseV3.getBody();
-    assertNotNull(orderResponseV3.getId());
-    assertEquals("myOrder", orderResponseV3.getName());
-    assertEquals(List.of("item1", "item2"), orderResponseV3.getItems());
+    assertNotNull(orderResponseV3.get("id"));
+    assertEquals("myOrder", orderResponseV3.get("name"));
+    assertEquals(List.of("item1", "item2"), orderResponseV3.get("items"));
     assertEquals(
         List.of(
-            new OrderResponseV3.ItemResponseV3(1L, "item1"),
-            new OrderResponseV3.ItemResponseV3(1L, "item2")
+            Map.of("id", 1, "name", "item1"),
+            Map.of("id", 1, "name", "item2")
         ),
-        orderResponseV3.getItemsV2()
+        orderResponseV3.get("itemsV2")
     );
 
     // вызываем v3 так, как будто это v2
     final var responseV2 = rest.postForEntity(
         "/api/order/v3",
-        new OrderRequestV2("anotherOrder", List.of("item1")),
-        OrderResponseV2.class
+        Map.of(
+            "name", "anotherOrder",
+            "items", List.of("item1")
+        ),
+        Map.class
     );
-    assertTrue(responseV2.getStatusCode().is2xxSuccessful(), "Unexpected status code: " + responseV2.getStatusCode());
+    assertTrue(responseV2.getStatusCode().is2xxSuccessful(),
+        "Unexpected status code: " + responseV2.getStatusCode());
     final var orderResponseV2 = responseV2.getBody();
-    assertNotNull(orderResponseV2.getId());
-    assertEquals("anotherOrder", orderResponseV2.getName());
-    assertEquals(List.of("item1"), orderResponseV2.getItems());
+    assertNotNull(orderResponseV2.get("id"));
+    assertEquals("anotherOrder", orderResponseV2.get("name"));
+    assertEquals(List.of("item1"), orderResponseV2.get("items"));
 
+    // Вызываем v3 так, как будто это v1
     final var responseV1 = rest.postForEntity(
         "/api/order/v3",
-        new OrderRequestV1("some order"),
-        OrderResponseV1.class
+        Map.of(
+            "name", "some order"
+        ),
+        Map.class
     );
-    assertTrue(responseV1.getStatusCode().is2xxSuccessful(), "Unexpected status code: " + responseV1.getStatusCode());
+    assertTrue(responseV1.getStatusCode().is2xxSuccessful(),
+        "Unexpected status code: " + responseV1.getStatusCode());
     final var orderResponseV1 = responseV1.getBody();
-    assertNotNull(orderResponseV1.getId());
-    assertEquals("some order", orderResponseV1.getName());
+    assertNotNull(orderResponseV1.get("id"));
+    assertEquals("some order", orderResponseV1.get("name"));
   }
 }
